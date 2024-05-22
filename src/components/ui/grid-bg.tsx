562a022643dbf3e52fc16abe489a299cb53a0674
Daniel Marques <daniel.brz2009@gmail.com>
2024-05-21 22:14:24 -0300

    remove unused dep

--- src/components/ui/grid-bg.tsx
+++
@@ -1,9 +0,0 @@
-import React, { type ReactNode } from "react"
-
-export function GridBackground({ children }: { children: ReactNode }) {
-  return (
-    <div className="relative flex w-full items-center justify-center bg-white bg-grid-small-black/[0.1] dark:bg-black dark:bg-grid-small-white/[0.2]">
-      {children}
-    </div>
-  )
-}
